494f8d0e3eedc5f7df13873701bbbd3f804e89c0
ronaldjc <ronald.jc@hotmail.com>
2022-10-28 12:21:26 -0300

    create first router

---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,9 @@
+import { Request, Response, Router } from 'express';
+
+const routes = Router();
+
+routes.get('/', (_: Request, response: Response) => {
+  return response.json({ message: 'Hello Word' });
+});
+
+export default routes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,3 +1,13 @@
-console.log(`Hello World!`);
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
-const test = 0;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () =>
+  console.log(`Server is running at http://localhost:3333`),
+);
