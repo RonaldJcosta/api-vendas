bc880b2c837e6379b9ca3a6ad466e7386725398e
ronaldjc <ronald.jc@hotmail.com>
2022-10-28 13:42:35 -0300

    change single quote to crasis

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,7 +3,7 @@ import { Request, Response, Router } from 'express';
 const routes = Router();
 
 routes.get('/', (_: Request, response: Response) => {
-  return response.json({ message: 'Hello Word' });
+  return response.json({ message: `Hello Word` });
 });
 
 export default routes;
